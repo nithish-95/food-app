0306ce8f9a6b671b93bfd449844d8b39bac40704
Nithish S <nithish952001@gmail.com>
2024-07-05 19:24:07 -0700

    initial Commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// GET https://api.spoonacular.com/recipes/complexSearch
+func getRecipes(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", get)
+	http.ListenAndServe(":3000", r)
+}
